5ca0828674e3f02667374ed21df51f685f002b42
blackbuffett <j.tucker0114@gmail.com>
2025-08-29 00:46:09 -0400

    Update config.js

--- EveryDollarInvested_Site/js/config.js
+++ EveryDollarInvested_Site/js/config.js
@@ -1,17 +1,10 @@
 window.BABYCOINZ_CONFIG = {
-  marketProvider: 'demo',
+  marketProvider: 'alphavantage',
   newsProvider: 'demo',
-  apiKeys: { alphavantage:'', finnhub:'', polygon:'', newsapi:'' },
+  apiKeys: { 
+    alphavantage:'2VYR7VDQD66A3R1V', 
+    finnhub:'', 
+    polygon:'', 
+    newsapi:'' 
+  },
   defaultSymbols: ['VOO','VTI','QQQ','SCHD','SPY','IJR'],
-
-  // ✅ Waitlist wired to ConvertKit
-  waitlist: {
-    provider: 'convertkit',
-    endpoints: {
-      convertkit: {
-        action: 'https://api.convertkit.com/v3/forms/8494037/subscribe',
-        api_key: 'h-8NE1V4SLGUOu6-lJeEyQ'   
-      }
-    }
-  }
-};
